ac0e1fb6ae322f91a26783627a3efb558483f22c
Antwane <antoinedespres13@gmail.com>
2023-04-28 18:03:56 +0200

    :lipstick: Navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,21 @@
 	import '@src/app.scss';
 	import Navbar from '$lib/interface/components/layout/navbar/navbar.component.svelte';
 	import type { PageData } from './$types';
-	export let data : PageData;
+	import { onMount } from 'svelte/internal';
+	import { navigating } from '$app/stores';
+	import { fly } from 'svelte/transition';
+	export let data: PageData;
+	let mounted: boolean = false;
+	onMount(() => {
+		mounted = true;
+	});
 </script>
+
+{#if !mounted || $navigating}
+	<!-- do something -->
+{/if}
+
 <Navbar {data} />
 
 <slot />
+
